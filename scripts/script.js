1a4a64ce3a9f5115826ab40883c7b0f0ce163f66
seop1284-kr <seop1284@naver.com>
2021-08-29 18:28:42 +0900

    Contact 콘텐츠 추가
    반응형 Contact 콘텐츠

--- scripts/script.js
+++ scripts/script.js
@@ -3,10 +3,40 @@ $(document).ready(function () {
     $(".nav-btn").each(function (idx) {
         $(this).click(function () {
             $('html,body').animate({
-                scrollTop: $("section").eq(idx).offset().top},
+                    scrollTop: $("section").eq(idx).offset().top
+                },
                 'slow');
         });
 
     });
+
+    // 이메일 복사 버튼
+    function copyToClipboard(val) {
+        var t = document.createElement("textarea");
+        document.body.appendChild(t);
+        t.value = val;
+        t.select();
+        document.execCommand('copy');
+        document.body.removeChild(t);
+    }
+
+    $('#email-copy-btn').click(function () {
+        copyToClipboard('seop1284@gmail.com');
+        $("#myModal").css("display", "block");
+
+    });
+
+
+    // When the user clicks on <span> (x), close the modal
+    $('.close').click(function () {
+        $("#myModal").css("display", "none");
+    });
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == document.getElementById("myModal")) {
+            document.getElementById("myModal").style.display = "none";
+        }
+    }
 
 });
